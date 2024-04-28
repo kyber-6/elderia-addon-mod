516ac6d197c0d17f055f2fc750efcee075686232
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-04-28 11:42:04 +0200

    remove example spell & item

--- src/main/java/net/pixeldreamstudios/elderia_addon/registry/ItemRegistry.java
+++ src/main/java/net/pixeldreamstudios/elderia_addon/registry/ItemRegistry.java
@@ -1,6 +1,5 @@
 package net.pixeldreamstudios.elderia_addon.registry;
 
-import io.redspace.ironsspellbooks.api.registry.SpellDataRegistryHolder;
 import net.minecraft.world.item.ArmorItem.Type;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -8,7 +7,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.pixeldreamstudios.elderia_addon.ElderiaAddon;
-import net.pixeldreamstudios.elderia_addon.item.ExampleMagicSword;
 import net.pixeldreamstudios.elderia_addon.item.armor.sets.CrusaderArmorItem;
 
 public class ItemRegistry {
@@ -18,7 +16,6 @@ public class ItemRegistry {
         ITEMS.register(eventBus);
     }
 
-    public static final RegistryObject<Item> EXAMPLE_MAGIC_SWORD = ITEMS.register("example_magic_sword", () -> new ExampleMagicSword(new SpellDataRegistryHolder[]{new SpellDataRegistryHolder(ExampleSpellRegistry.SUPER_HEAL_SPELL, 1)}));
     public static final RegistryObject<Item> CRUSADER_HELMET = ITEMS.register("crusader_helmet", () -> new CrusaderArmorItem(Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> CRUSADER_CHESTPLATE = ITEMS.register("crusader_chestplate", () -> new CrusaderArmorItem(Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> CRUSADER_LEGGINGS = ITEMS.register("crusader_leggings", () -> new CrusaderArmorItem(Type.LEGGINGS, new Item.Properties()));
